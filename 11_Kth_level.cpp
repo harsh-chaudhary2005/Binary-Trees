75803c7551f846e4f07f1953569560b8755f2c3f
Harsh Chaudhary <hc492250@gmail.com>
2026-09-24 06:03:13 +0530

    Add files via upload

---
+++ 11_Kth_level.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+        Node(int val){
+            data = val;
+            left = right = NULL;
+        }
+};
+static int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+    if(preorder[idx] == -1){  
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+    return root;
+}
+void KthLevel(Node* root, int k){
+    if(root == NULL) return;
+    if(k == 1){
+        cout << root->data << " " ;
+        return;
+    }
+    KthLevel(root->left, k-1);
+    KthLevel(root->right, k-1);
+}
+int main(){
+  vector<int> preorder = {1, 2, 7, -1, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+  Node* root = buildTree(preorder);
+  KthLevel(root, 2);
+  return 0;
+}
